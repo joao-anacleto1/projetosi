86a4c9ce18d3b0ca39ff6d11db247d71577f94e3
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2024-05-08 03:35:57 +0100

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 import os
-import time  # Importe o módulo time
+import time
 
 app = Flask(__name__)
 CORS(app)
